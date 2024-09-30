0ba8ed4a997c63bb2647dfb52f7f83f3663316a4
hrmrakib <hrmrakibs@gmail.com>
2024-09-30 09:53:38 +0600

    create product slice

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,13 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialProduct = {products: []}
+const initialProduct = {
+    products: [],
+    status: "idle",
+    error: null
+}
 
 const productSlice = createSlice( {
     name: "products",
     initialState:initialProduct,
-    reducers:()=> {
-        addProduct: (state, action)=> {
-
+    reducers: {
+        fetchProductSuccess: (state, action)=> {
+            state.products = action.payload;
+            state.status = "success"
+        },
+        fetchProductFailed: (state, action)=> {
+            state.status = "failed"
+            state.error = action.payload;
+        },
+        fetchProductLoading: (state) => {
+            state.status = "loading"
         }
     }
 })
+
+export const {fetchProductSuccess,fetchProductFailed,fetchProductLoading} = productSlice.actions;
+
+export default productSlice.reducer
